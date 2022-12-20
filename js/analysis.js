39d62009dfae16d1702c5d9792c2f964dc937d3b
youxt-njnu <10190208@njnu.edu.cn>
2022-12-20 22:03:27 +0800

    improvenew1220

--- js/analysis.js
+++ js/analysis.js
@@ -7,7 +7,7 @@ var valueFromPageA = decodeURI(window.location.search);
 console.log("valueFromPageA: " + valueFromPageA);
 var reg = valueFromPageA.split("=");
 // console.log(reg[1]);
-var chosedText = reg[1];
+var chosedText = reg[1].trim();
 
 //页面
 var view = new ol.View({
@@ -17,26 +17,26 @@ var view = new ol.View({
   zoom: 11,
 });
 
-//----------geoserver发布的WTMS底图,其实数据源是4326坐标系的，但是geoserver会适配前端的坐标系。
-var topiclayer = new ol.layer.Image({
-  title: "land84",
-  source: new ol.source.ImageWMS({
-    ratio: 1,
-    url: "http://localhost:8080/geoserver/cite/wms?", //这个可以打开geoserver的preview，看openlayer页面截取url
-    // 请求参数
-    params: {
-      SERVICE: "WMS",
-      VERSION: "1.1.1",
-      REQUEST: "GetMap",
-      FORMAT: "image/png",
-      TRANSPARENT: true,
-      tiled: true,
-      LAYERS: "cite:land84", //图层，前面是工作空间，后面是图层名，
-      exceptions: "application/vnd.ogc.se_inimage",
-      singleTile: true, //单瓦片，渲染成一张图片
-    },
-  }),
-});
+// //----------geoserver发布的WTMS底图,其实数据源是4326坐标系的，但是geoserver会适配前端的坐标系。
+// var topiclayer = new ol.layer.Image({
+//   title: "land84",
+//   source: new ol.source.ImageWMS({
+//     ratio: 1,
+//     url: "http://localhost:8080/geoserver/cite/wms?", //这个可以打开geoserver的preview，看openlayer页面截取url
+//     // 请求参数
+//     params: {
+//       SERVICE: "WMS",
+//       VERSION: "1.1.1",
+//       REQUEST: "GetMap",
+//       FORMAT: "image/png",
+//       TRANSPARENT: true,
+//       tiled: true,
+//       LAYERS: "cite:land84", //图层，前面是工作空间，后面是图层名，
+//       exceptions: "application/vnd.ogc.se_inimage",
+//       singleTile: true, //单瓦片，渲染成一张图片
+//     },
+//   }),
+// });
 
 // 加载mapbox底图和geoserver发布的WTMS底图
 var layers = [
@@ -76,7 +76,6 @@ var layers = [
       url: "https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGlua2xpbmsiLCJhIjoiY2t5azNveG05MnRwdTJ4bzhxM2JmNGg3aCJ9.giuzL5T9qkSSl9EWMUK9dg",
     }),
   }),
-  topiclayer,
 ];
 
 //地图
@@ -87,136 +86,201 @@ var map = new ol.Map({
 });
 
 //切换底图
-function openMenu() {
-  console.log("ok");
-  const layerList = document.getElementById("menu");
-  layerList.style.display = "inline";
-  layerList.addEventListener("click", (event) => {
-    if (event.target.checked) {
-      // 如果选中某一复选框
-      // 通过DOM元素的id值来判断应该对哪个图层进行显示
-      for (ie = 0; ie < 5; ie++) {
-        map.getLayers().item(ie).setVisible(false);
-      }
-      switch (event.target.id) {
-        case "satellite-v9":
-          map.getLayers().item(0).setVisible(true);
-          break;
-        case "light-v10":
-          map.getLayers().item(1).setVisible(true);
-          break;
-        case "dark-v10":
-          map.getLayers().item(2).setVisible(true);
-          break;
-        case "streets-v11":
-          map.getLayers().item(3).setVisible(true);
-          break;
-        case "outdoors-v11":
-          map.getLayers().item(4).setVisible(true);
-          break;
-        default:
-          break;
-      }
-      map.getLayers().item(5).setVisible(true);
+let layerList = document.getElementById("menu");
+layerList.addEventListener("click", (event) => {
+  if (event.target.checked) {
+    // 如果选中某一复选框
+    // 通过DOM元素的id值来判断应该对哪个图层进行显示
+    for (ie = 0; ie < 5; ie++) {
+      map.getLayers().item(ie).setVisible(false);
     }
+    switch (event.target.id) {
+      case "satellite-v9":
+        map.getLayers().item(0).setVisible(true);
+        break;
+      case "light-v10":
+        map.getLayers().item(1).setVisible(true);
+        break;
+      case "dark-v10":
+        map.getLayers().item(2).setVisible(true);
+        break;
+      case "streets-v11":
+        map.getLayers().item(3).setVisible(true);
+        break;
+      case "outdoors-v11":
+        map.getLayers().item(4).setVisible(true);
+        break;
+      default:
+        break;
+    }
+    map.getLayers().item(5).setVisible(true);
+  }
+});
+
+var referL = {
+  四川省: ["road84", "basecounty"],
+  土地利用: ["land84"],
+  "1900至今": ["land84", "poi84"],
+};
+var Trefer = {
+  basecounty: "成都市部分县级面数据",
+  road84: "成都市部分路网数据",
+  land84: "成都市部分土地利用数据",
+  poi84: "成都市部分POI数据",
+};
+
+var wfsVectorLayer = null;
+
+function GetData(a) {
+  wfsVectorLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      format: new ol.format.GeoJSON({
+        geometryName: "the_geom",
+      }),
+      url:
+        "http://localhost:8080/geoserver/wfs?service=wfs&version=1.1.0&request=GetFeature&typeNames=cite:" +
+        a +
+        "&outputFormat=application/json&srsname=EPSG:4326",
+    }),
   });
 }
+
+var searchQueryElem = document.getElementById("js-search-query");
+
+searchQueryElem.addEventListener("keyup", function () {
+  wfsVectorLayer.changed();
+  wfsVectorLayer.setStyle(styleFunction);
+});
+
+var styleFunction = function (feature) {
+  var cityName = feature.get("NAME");
+  var searchQuery = searchQueryElem.value;
+  var opacity = 1;
+
+  if (searchQuery && cityName.search(new RegExp(searchQuery, "i")) === -1) {
+    opacity = 0.3;
+  }
+
+  return [
+    new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: [128, 128, 128, opacity - 0.3],
+      }),
+      stroke: new ol.style.Stroke({
+        color: [192, 0, 0, opacity - 0.3],
+        width: 1,
+      }),
+      text: new ol.style.Text({
+        text: cityName,
+        fill: new ol.style.Fill({
+          color: [192, 0, 0, opacity - 0.3],
+        }),
+        stroke: new ol.style.Stroke({
+          color: [255, 255, 255, opacity - 0.3],
+          width: 1,
+        }),
+        font: '11px "Helvetica Neue", Arial',
+      }),
+    }),
+  ];
+};
 
 //地图点击事件
 //线上线下访问url不同，可变配置提出
-var baseurl = "http://localhost:8080/";
-$("#map").click(function (e) {
-  //获取地图上点击的地理坐标，平面墨卡托坐标系
-  var t3857 = map.getEventCoordinate(e.originalEvent);
-  console.log(t3857);
-  t4326 = ol.proj.transform(t3857, "EPSG:3857", "EPSG:4326");
-  console.log(t4326);
-  // BBOX,minlng,minlat,maxlng,maxlat,平面墨卡托就是minx,miny,maxx,maxy，下面的url3857里的bbox是一个2m×2m的小矩形
-  //构造请求url的时候，把坐标系写成3857，虽然后台数据是4326坐标系的，但geoserver能内部转换
-  var url3857 =
-    baseurl +
-    "geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=cite%3Aland84&LAYERS=cite%3Aland84&exceptions=application%2Fvnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50&X=50&Y=50&SRS=EPSG%3A3857&STYLES=&WIDTH=101&HEIGHT=101&BBOX=" +
-    (t3857[0] - 1).toString() +
-    "%2C" +
-    (t3857[1] - 1).toString() +
-    "%2C" +
-    (t3857[0] + 1).toString() +
-    "%2C" +
-    (t3857[1] + 1).toString();
-  console.log(url3857);
-  $.ajax({
-    url: url3857,
-    type: "GET",
-    dataType: "json",
-    headers: { "Content-Type": "application/json;charset=utf8" },
-    success: function (data) {
-      //这个方法直接把geojson转为feature数组
-      features = new ol.format.GeoJSON().readFeatures(data);
-      //新建矢量资源
-      var vectorSource = new ol.source.Vector({
-        features: features,
-      });
-      //新建矢量图层
-      var vectorLayer = new ol.layer.Vector({
-        source: vectorSource,
-        style: polygonStyleFunction,
-      });
-      //将矢量图层添加到map
-      map.addLayer(vectorLayer);
-      //更新属性表
-      for (var i = 0; i < data["features"].length; i++) {
-        var properties = data["features"][i]["properties"];
-        var idHD = properties["id_0"];
-        var categoryHD = properties["les-miserables_category"];
-        var nameHD = properties["les-miserables_name"];
-        var valueHD = properties["nles-miserables_value"];
+// var baseurl = "http://localhost:8080/";
+// $("#map").click(function (e) {
+//   //获取地图上点击的地理坐标，平面墨卡托坐标系
+//   var t3857 = map.getEventCoordinate(e.originalEvent);
+//   console.log(t3857);
+//   t4326 = ol.proj.transform(t3857, "EPSG:3857", "EPSG:4326");
+//   console.log(t4326);
+//   // BBOX,minlng,minlat,maxlng,maxlat,平面墨卡托就是minx,miny,maxx,maxy，下面的url3857里的bbox是一个2m×2m的小矩形
+//   //构造请求url的时候，把坐标系写成3857，虽然后台数据是4326坐标系的，但geoserver能内部转换
+//   var url3857 =
+//     baseurl +
+//     "geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image%2Fpng&TRANSPARENT=true&QUERY_LAYERS=cite%3Aland84&LAYERS=cite%3Aland84&exceptions=application%2Fvnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=50&X=50&Y=50&SRS=EPSG%3A3857&STYLES=&WIDTH=101&HEIGHT=101&BBOX=" +
+//     (t3857[0] - 1).toString() +
+//     "%2C" +
+//     (t3857[1] - 1).toString() +
+//     "%2C" +
+//     (t3857[0] + 1).toString() +
+//     "%2C" +
+//     (t3857[1] + 1).toString();
+//   console.log(url3857);
+//   $.ajax({
+//     url: url3857,
+//     type: "GET",
+//     dataType: "json",
+//     headers: { "Content-Type": "application/json;charset=utf8" },
+//     success: function (data) {
+//       //这个方法直接把geojson转为feature数组
+//       features = new ol.format.GeoJSON().readFeatures(data);
+//       //新建矢量资源
+//       var vectorSource = new ol.source.Vector({
+//         features: features,
+//       });
+//       //新建矢量图层
+//       var vectorLayer = new ol.layer.Vector({
+//         source: vectorSource,
+//         style: polygonStyleFunction,
+//       });
+//       //将矢量图层添加到map
+//       map.addLayer(vectorLayer);
+//       //更新属性表
+//       for (var i = 0; i < data["features"].length; i++) {
+//         var properties = data["features"][i]["properties"];
+//         var idHD = properties["id_0"];
+//         var categoryHD = properties["les-miserables_category"];
+//         var nameHD = properties["les-miserables_name"];
+//         var valueHD = properties["nles-miserables_value"];
 
-        var tabletxt = "<tr><td>" + idHD + "</td><td>";
-        categoryHD +
-          "</td><td>" +
-          nameHD +
-          "</td><td>" +
-          valueHD +
-          "</td></tr>";
-        $("#attributetbody").append(tabletxt);
-      }
-    },
-    error: function (data) {
-      console.log("faile");
-      console.log(data);
-    },
-  });
-});
-//制图风格，标注内容要从要素中获取，每个要素的name_ch属性不同，所以制图风格是方法，而不是静态的
-function polygonStyleFunction(feature) {
-  return new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: "rgba(192, 0, 0, 1)",
-      width: 2,
-    }),
-    fill: new ol.style.Fill({
-      color: "rgba(192, 192, 192, 0.5)",
-    }),
-    text: createTextStyle(feature),
-  });
-}
-//创建注记
-function createTextStyle(feature) {
-  return new ol.style.Text({
-    font: "20px Microsoft YaHei",
-    text: getText(feature),
-    fill: new ol.style.Fill({
-      color: "rgba(192, 0, 0, 1)",
-    }),
-    stroke: new ol.style.Stroke({ color: "rgba(255, 255, 255, 1)", width: 1 }),
-  });
-}
-//获取要素属性内容
-function getText(feature) {
-  return feature.get("les-miserables_name").toString();
-}
+//         var tabletxt = "<tr><td>" + idHD + "</td><td>";
+//         categoryHD +
+//           "</td><td>" +
+//           nameHD +
+//           "</td><td>" +
+//           valueHD +
+//           "</td></tr>";
+//         $("#attributetbody").append(tabletxt);
+//       }
+//     },
+//     error: function (data) {
+//       console.log("faile");
+//       console.log(data);
+//     },
+//   });
+// });
+// //制图风格，标注内容要从要素中获取，每个要素的name_ch属性不同，所以制图风格是方法，而不是静态的
+// function polygonStyleFunction(feature) {
+//   return new ol.style.Style({
+//     stroke: new ol.style.Stroke({
+//       color: "rgba(192, 0, 0, 1)",
+//       width: 2,
+//     }),
+//     fill: new ol.style.Fill({
+//       color: "rgba(192, 192, 192, 0.5)",
+//     }),
+//     text: createTextStyle(feature),
+//   });
+// }
+// //创建注记
+// function createTextStyle(feature) {
+//   return new ol.style.Text({
+//     font: "20px Microsoft YaHei",
+//     text: getText(feature),
+//     fill: new ol.style.Fill({
+//       color: "rgba(192, 0, 0, 1)",
+//     }),
+//     stroke: new ol.style.Stroke({ color: "rgba(255, 255, 255, 1)", width: 1 }),
+//   });
+// }
+// //获取要素属性内容
+// function getText(feature) {
+//   return feature.get("les-miserables_name").toString();
+// }
 
 $(function () {
+  //模态框处理开始----
   $("#btn2").click(function () {
     // $("#myModal").modal({
     //     show:true,
@@ -242,56 +306,25 @@ $(function () {
   $("#myModal2").on("hidden.bs.modal", function () {
     console.log("完全隐藏之后执行2");
   });
-});
+  //模态框处理结束----
 
-var searchQueryElem = document.getElementById("js-search-query");
-
-searchQueryElem.addEventListener("keyup", function () {
-  vectorLayer.changed();
-});
-
-var styleFunction = function (feature) {
-  var cityName = feature.get("name");
-  var searchQuery = searchQueryElem.value;
-  var opacity = 1;
-
-  if (searchQuery && cityName.search(new RegExp(searchQuery, "i")) === -1) {
-    opacity = 0.3;
+  console.log(chosedText);
+  var inf = "";
+  for (var i = 0; i < referL[chosedText].length; i++) {
+    GetData(referL[chosedText][i]);
+    map.addLayer(wfsVectorLayer);
+    inf =
+      inf +
+      "<input value=" +
+      referL[chosedText][i] +
+      ' type="radio" name="Llist" /><label for=' +
+      referL[chosedText][i] +
+      ">" +
+      Trefer[referL[chosedText][i]] +
+      "</label><br />";
   }
-
-  return [
-    new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 10,
-        stroke: new ol.style.Stroke({
-          color: [74, 138, 168, opacity],
-          width: 2,
-        }),
-      }),
-      text: new ol.style.Text({
-        text: cityName,
-        fill: new ol.style.Fill({
-          color: [255, 255, 255, opacity],
-        }),
-        stroke: new ol.style.Stroke({
-          color: [0, 51, 0, opacity],
-          width: 1,
-        }),
-        font: '11px "Helvetica Neue", Arial',
-      }),
-    }),
-  ];
-};
-
-var vectorLayer = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: "cities.geojson",
-    format: new ol.format.GeoJSON(),
-  }),
-  style: styleFunction,
+  $("#chooseData").append(inf);
 });
-
-map.addLayer(vectorLayer);
 
 //TODO 这个里面要有点击数据，弹出标签，然后里面引导出内容这一部分
 
